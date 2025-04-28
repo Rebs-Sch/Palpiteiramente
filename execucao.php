71a379166f889a3b69e4a722f81a51eb52de02a4
Re <134728551+Rebs-Sch@users.noreply.github.com>
2025-04-28 00:03:51 -0300

    CORREÇÃO

--- execucao.php
+++ execucao.php
@@ -42,8 +42,8 @@ if ($palpite == $certo) {
     <h3>Na verdade, o personagem sorteado era:</h3>
     
     <div class='personagem'>
-        <img src='".$palpites[$palpite-1]->getImg()."'>
-        <h4>".$palpites[$palpite-1]->getNome()."</h4>
+        <img src='".$palpites[$certo-1]->getImg()."'>
+        <h4>".$palpites[$certo-1]->getNome()."</h4>
     </div>
     
     <p>Boa tentativa, mas infelizmente essa não era a alternativa correta.</p>
